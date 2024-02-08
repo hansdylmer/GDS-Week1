00a8eafd4fe44da4264c6aa25273bd0e27aae928
Hans Dylmer <hdylmer@gmail.com>
2024-02-08 15:45:11 +0100

    Added string

--- simple_classifier.py
+++ simple_classifier.py
@@ -5,6 +5,7 @@ from matplotlib.colors import ListedColormap
 from sklearn import datasets, neighbors
 from sklearn.inspection import DecisionBoundaryDisplay
 
+str = "String"
 if __name__ == "__main__":
     n_neighbors = 15
 
